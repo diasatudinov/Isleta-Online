eb2c3d2eecf956a32a65d5e313f40254583617e0
Dias <dias.atudinov@gmail.com>
2024-12-16 16:23:47 +0500

    first init

---
+++ Isleta Online/Isleta Online/Isleta_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Isleta_OnlineApp.swift
+//  Isleta Online
+//
+//  Created by Dias Atudinov on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Isleta_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
