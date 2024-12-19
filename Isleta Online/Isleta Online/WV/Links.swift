cfd76087cfb32a3ed793dd1121d56334b9f7717d
Dias <dias.atudinov@gmail.com>
2024-12-19 19:28:23 +0500

    add links

--- Isleta Online/Isleta Online/WV/Links.swift
+++ Isleta Online/Isleta Online/WV/Links.swift
@@ -13,7 +13,7 @@ class Links {
     static let shared = Links()
     
     static let winStarData = "https://onlinews.xyz/info"
-    static let privacyData = "https://onlinews.xyz/"
+   // static let privacyData = "https://onlinews.xyz/"
     
     @AppStorage("finalUrl") var finalURL: URL?
     
